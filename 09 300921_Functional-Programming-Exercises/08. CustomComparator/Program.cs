2578e502439146175f79b173af8655489b6cb9c7
Stefan Bonev <bonev@x-spot.org>
2021-10-14 23:55:29 +0300

    09 300921

--- 09 300921_Functional-Programming-Exercises/08. CustomComparator/Program.cs
+++ 09 300921_Functional-Programming-Exercises/08. CustomComparator/Program.cs
@@ -27,3 +27,12 @@ namespace _08._CustomComparator
         }
     }
 }
+/*
+var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Func<int, int, int> comparator = (number1, number2) =>
+    (number1 % 2 == 0 && number2 % 2 != 0) ? -1 :
+    (number1 % 2 != 0 && number2 % 2 == 0) ? 1 :
+    number1.CompareTo(number2);
+Array.Sort<int>(numbers, new Comparison<int>(comparator));
+Console.WriteLine(string.Join(" ", numbers));
+*/
