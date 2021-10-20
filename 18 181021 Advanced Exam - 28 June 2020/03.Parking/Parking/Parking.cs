9c4ebcff2aac3727997676af7abc2ff8ed2d65d4
Stefan Bonev <bonev@x-spot.org>
2021-10-20 21:09:31 +0300

    18 181021

---
+++ 18 181021 Advanced Exam - 28 June 2020/03.Parking/Parking/Parking.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parking
+{
+    class Parking
+    {
+        private List<Car> data;
+        public Parking(string type, int capacity)
+        {
+            Type = type;
+            Capacity = capacity;
+            data = new List<Car>();
+        }
+
+        public string Type { get; set; }
+        public int Capacity { get; set; }
+        public int Count { get { return data.Count; } }        
+
+        public void Add(Car car)
+        {
+            if (Capacity >= data.Count + 1)
+            {
+                data.Add(car);
+            }
+        }
+
+        public bool Remove(string manufacturer, string model)
+        {
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (data[i].Manufacturer == manufacturer &&
+                    data[i].Model == model)
+                {
+                    data.RemoveAt(i);
+                    return true;
+                }
+            }
+
+            return false;
+            Car carToRemove = data.FirstOrDefault(c =>
+                        c.Manufacturer == manufacturer
+                        && c.Model == model);
+            if (carToRemove != null)
+            {
+                data.Remove(carToRemove);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Car GetLatestCar()
+        {
+            return data.OrderByDescending(x => x.Year).FirstOrDefault();
+        }
+
+        public Car GetCar(string manufacturer, string model)
+        {
+            Car car = data.FirstOrDefault(c => c.Manufacturer == manufacturer && c.Model == model);
+            return car;
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"The cars are parked in {Type}:");
+            foreach (var car in data)
+            {
+                result.AppendLine(car.ToString());
+            }
+
+            return result.ToString();
+        }
+    }
+}
