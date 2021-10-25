5d914d480183d12e7a934baffaaf68678f85fe8b
Stefan Bonev <bonev@x-spot.org>
2021-10-25 19:25:33 +0300

    21 231021

---
+++ 21 231021 CSharp Advanced Exam - 23.10.21/02.PawnWars/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+
+namespace _02._Pawn_Wars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] classboard = new string[8, 8];
+            for (int i = 7; i >= 0; i--)
+            {
+                string row = Console.ReadLine();
+                for (int col = 0; col < 8; col++)
+                {
+                    classboard[i, col] = row[col].ToString();
+                }
+            }
+            while (true)
+            {
+                int rowW = GetRow(classboard, "w");
+                int colW = GetCol(classboard, "w");
+                int rowB = GetRow(classboard, "b");
+                int colB = GetCol(classboard, "b");
+                char letterCol = '\0';
+                if (colW + 1 < 8 && colW - 1 >= 0)
+                {
+                    if (classboard[rowW + 1, colW + 1] == "b" || classboard[rowW + 1, colW - 1] == "b")
+                    {
+                        letterCol = GetLetterCol(classboard, colB);
+                        Console.WriteLine($"Game over! White capture on {letterCol}{rowW + 2}.");
+                        break;
+                    }
+                }
+                else if (colW + 1 < 8 && colW - 1 < 0)
+                {
+                    if (classboard[rowW + 1, colW + 1] == "b")
+                    {
+                        letterCol = GetLetterCol(classboard, colB);
+                        Console.WriteLine($"Game over! White capture on {letterCol}{rowW + 2}.");
+                        break;
+                    }
+                }
+                else if (colW + 1 >= 8 && colW - 1 >= 0)
+                {
+                    if (classboard[rowW + 1, colW - 1] == "b")
+                    {
+                        letterCol = GetLetterCol(classboard, colB);
+                        Console.WriteLine($"Game over! White capture on {letterCol}{rowW + 2}.");
+                        break;
+                    }
+                }
+                classboard[rowW, colW] = "-";
+                rowW += 1;
+                if (rowW == 7)
+                {
+                    letterCol = GetLetterCol(classboard, colW);
+                    Console.WriteLine($"Game over! White pawn is promoted to a queen at {letterCol}{rowW + 1}.");
+                    break;
+                }
+                classboard[rowW, colW] = "w";
+                classboard[rowB, colB] = "-";
+                if (colB + 1 < 8 && colB - 1 >= 0)
+                {
+                    if (classboard[rowB - 1, colB - 1] == "w" || classboard[rowB - 1, colB] == "w")
+                    {
+                        letterCol = GetLetterCol(classboard, colW);
+                        Console.WriteLine($"Game over! Black capture on {letterCol}{rowB}.");
+                        break;
+                    }
+                }
+                else if (colB + 1 < 8 && colB - 1 < 0)
+                {
+                    if (classboard[rowB - 1, colB + 1] == "w")
+                    {
+                        letterCol = GetLetterCol(classboard, colW);
+                        Console.WriteLine($"Game over! Black capture on {letterCol}{rowB}.");
+                        break;
+                    }
+                }
+                else if (colB + 1 >= 8 && colB - 1 >= 0)
+                {
+                    if (classboard[rowB - 1, colB - 1] == "w")
+                    {
+                        letterCol = GetLetterCol(classboard, colW);
+                        Console.WriteLine($"Game over! Black capture on {letterCol}{rowB}.");
+                        break;
+                    }
+                }
+                rowB -= 1;
+                if (rowB == 0)
+                {
+                    letterCol = GetLetterCol(classboard, colB);
+                    Console.WriteLine($"Game over! Black pawn is promoted to a queen at {letterCol}{rowB + 1}.");
+                    break;
+                }
+                classboard[rowB, colB] = "b";
+            }
+        }
+
+        private static char GetLetterCol(string[,] classboard, int colW)
+        {
+            if (colW == 0)
+            {
+                return 'a';
+            }
+            else if (colW == 1)
+            {
+                return 'b';
+            }
+            else if (colW == 2)
+            {
+                return 'c';
+            }
+            else if (colW == 3)
+            {
+                return 'd';
+            }
+            else if (colW == 4)
+            {
+                return 'e';
+            }
+            else if (colW == 5)
+            {
+                return 'f';
+            }
+            else if (colW == 6)
+            {
+                return 'g';
+            }
+            else if (colW == 7)
+            {
+                return 'h';
+            }
+            return '\0';
+        }
+
+        private static int GetCol(string[,] classboard, string v)
+        {
+            for (int row = 0; row < 8; row++)
+            {
+                for (int col = 0; col < 8; col++)
+                {
+                    if (classboard[row, col] == v)
+                    {
+                        return col;
+                    }
+                }
+            }
+            return 0;
+        }
+
+        private static int GetRow(string[,] classboard, string v)
+        {
+            for (int row = 0; row < 8; row++)
+            {
+                for (int col = 0; col < 8; col++)
+                {
+                    if (classboard[row, col] == v)
+                    {
+                        return row;
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ 21 231021 CSharp Advanced Exam - 23.10.21/03.StockMarket/Investor.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockMarket
+{
+    public class Investor
+    {
+        private HashSet<Stock> portfolio;
+        public Investor(string fullName, string emailAddress, decimal moneyToInvest, string brokerName)
+        {
+            portfolio = new HashSet<Stock>();
+            FullName = fullName;
+            EmailAddress = emailAddress;
+            MoneyToInvest = Math.Abs(moneyToInvest);
+            BrokerName = brokerName;
+        }
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal MoneyToInvest { get; set; }
+        public string BrokerName { get; set; }
+        public int Count { get { return portfolio.Count; } }
+        public void BuyStock(Stock stock)
+        {
+            if (stock.MarketCapitalization > 10000 && stock.PricePerShare <= MoneyToInvest)
+            {
+                MoneyToInvest -= stock.PricePerShare;
+                portfolio.Add(stock);
+            }
+        }
+        public string SellStock(string companyName, decimal sellPrice)
+        {
+            if (CompanyExist(companyName))
+            {
+                Stock currentStock = FindStock(companyName);
+                if (sellPrice < currentStock.PricePerShare)
+                {
+                    return $"Cannot sell {companyName}.";
+                }
+                else
+                {
+                    portfolio.Remove(currentStock);
+                    MoneyToInvest += sellPrice;
+                    return $"{companyName} was sold.";
+                }
+            }
+            else
+            {
+                return $"{companyName} does not exist.";
+            }
+        }
+        public Stock FindStock(string companyName)
+        {
+            foreach (var stock in portfolio)
+            {
+                if (stock.CompanyName == companyName)
+                {
+                    return stock;
+                }
+            }
+            return null;
+        }
+        public Stock FindBiggestCompany()
+        {
+            return portfolio.OrderByDescending(s => s.MarketCapitalization).FirstOrDefault();
+        }
+        public string InvestorInformation()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The investor {FullName} with a broker {BrokerName} has stocks:");
+            foreach (var stock in portfolio)
+            {
+                sb.AppendLine(stock.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+        private bool CompanyExist(string companyName)
+        {
+            foreach (var stock in portfolio)
+            {
+                if (stock.CompanyName == companyName)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
