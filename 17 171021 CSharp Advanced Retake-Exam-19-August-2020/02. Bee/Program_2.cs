70868b1cb26099a6c239e489c9a95671bc110ce6
Stefan Bonev <bonev@x-spot.org>
2022-03-13 19:27:07 +0200

    17 171021

---
+++ 17 171021 CSharp Advanced Retake-Exam-19-August-2020/02. Bee/Program_2.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace _02._Bee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                string currentRow = Console.ReadLine();
+                for (int col = 0; col < currentRow.Length; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            int pollflowers = 0;
+            int beeRow = 0;
+            int beeCol = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (matrix[row,col] == 'B')
+                    {
+                        beeRow = row;
+                        beeCol = col;
+                    }
+                }
+            }
+
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                matrix[beeRow, beeCol] = '.';
+                beeRow = MoveRow(beeRow, command);
+                beeCol = MoveCol(beeCol, command);
+
+                if (!IsValidPosition(beeRow, beeCol, n,n))
+                {
+                    Console.WriteLine("The bee got lost!");
+                    break;
+                }
+
+                if (matrix[beeRow, beeCol] == 'f')
+                {
+                    pollflowers++;
+                }
+
+                if (matrix[beeRow, beeCol] == 'O')
+                {
+                    matrix[beeRow, beeCol] = '.';
+                    beeRow = MoveRow(beeRow, command);
+                    beeCol = MoveCol(beeCol, command);
+                    if (matrix[beeRow, beeCol] == 'f')
+                    {
+                        pollflowers++;
+                    }
+                }
+
+
+                matrix[beeRow, beeCol] = 'B';
+                command = Console.ReadLine();
+            }
+
+            if (pollflowers < 5)
+            {
+                Console.WriteLine($"The bee couldn't pollinate the flowers, she needed {5 - pollflowers} flowers more");
+            }
+
+            else
+            {
+                Console.WriteLine($"Great job, the bee managed to pollinate {pollflowers} flowers!");
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static int MoveRow(int row, string movement)
+        {
+            if (movement == "up")
+            {
+                return row - 1;
+            }
+            if (movement == "down")
+            {
+                return row + 1;
+            }
+            return row;
+        }
+
+        public static int MoveCol(int col, string movement)
+        {
+            if (movement == "left")
+            {
+                return col - 1;
+            }
+            if (movement == "right")
+            { 
+                return col + 1;
+            }
+            return col;
+        }
+
+        public static bool IsValidPosition(int row, int col, int rows, int cols)
+        {
+            if (row < 0 || row >= rows || col < 0 || col >= cols)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
