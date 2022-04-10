c2698cdeb58bc1e9c834e96142eccb6ac0dff3a6
Stefan Bonev <bonev@x-spot.org>
2022-04-10 17:39:35 +0300

    25 100422

---
+++ 25 100422 CSharp Advanced Retake Exam - 16 December 2020/03.Opening/BakeryOpenning/Bakery.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Bakery
+    {
+        private List<Employee> data;
+        public Bakery(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            data = new List<Employee>();
+        }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count { get { return data.Count; } }
+
+        public void Add(Employee employee)
+        {
+            if (Capacity >= data.Count + 1)
+            {
+                data.Add(employee);
+            }
+        }
+        public bool Remove(string name)
+        {
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (data[i].Name == name)
+                {
+                    data.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public Employee GetOldestEmployee()
+        {
+            return data.OrderByDescending(e => e.Age).FirstOrDefault();
+        }
+        public Employee GetEmployee(string name)
+        {
+            Employee employee = data.FirstOrDefault(e => e.Name == name);
+            return employee;
+        }
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Employees working at Bakery {Name}:");
+            foreach (var employee in data)
+            {
+                sb.AppendLine(employee.ToString());
+            }
+            return sb.ToString();
+        }
+    }
+}
