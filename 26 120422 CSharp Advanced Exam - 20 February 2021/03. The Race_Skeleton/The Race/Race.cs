5ce529dc9037a62ad5a7df3982043bd4c5ffdc43
Stefan Bonev <bonev@x-spot.org>
2022-04-12 20:45:35 +0300

    26 120422

---
+++ 26 120422 CSharp Advanced Exam - 20 February 2021/03. The Race_Skeleton/The Race/Race.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheRace
+{
+    public class Race
+    {
+        private List<Racer> data;
+        public Race(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            data = new List<Racer>();
+        }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count { get { return data.Count; } }
+
+        public void Add(Racer racer)
+        {
+            if (data.Count < Capacity)
+            {
+                data.Add(racer);
+            }
+        }
+        public bool Remove(string name)
+        {
+            Racer racer = data.FirstOrDefault(r => r.Name == name);
+            if (racer == null)
+            {
+                return false;
+            }
+            data.Remove(racer);
+            return true;
+        }
+        public Racer GetOldestRacer()
+        {
+            return data.OrderByDescending(r => r.Age).FirstOrDefault();
+        }
+        public Racer GetRacer(string name)
+        {
+            Racer racer = data.FirstOrDefault(r => r.Name == name);
+            return racer;
+        }
+        public Racer GetFastestRacer()
+        {
+            return data.OrderByDescending(r => r.Car.Speed).FirstOrDefault();
+        }
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Racers participating at {Name}:");
+
+            for (int i = 0; i < data.Count - 1; i++)
+            {
+                sb.AppendLine(data[i].ToString());
+            }
+            sb.Append(data[data.Count - 1]);
+            return sb.ToString();
+        }
+    }
+}
